8cdd1eddb9cd72b5872febc0acde22ae07bd7293
CTMENDOZA <tiptop1112@gmail.com>
2022-02-14 19:53:53 -0800

    Completed javvascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,25 @@
+var fullName = 'Chris Mendoza';
+
+console.log('My name is: ', fullName);
+
+var isCool = true;
+
+console.log('Chris is cool?: ', isCool);
+
+var totalPets = 0;
+
+console.log('totalPets: ', totalPets);
+
+console.log(typeof fullName);
+
+console.log(typeof isCool);
+
+console.log(typeof totalPets);
+
+var unknown = null;
+
+console.log(typeof unknown);
+
+var never;
+
+console.log(typeof never);
