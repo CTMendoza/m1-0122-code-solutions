4a1bfcdd75a860c1e6cd470b0e808caddcde4c95
CTMENDOZA <tiptop1112@gmail.com>
2022-02-28 14:54:30 -0800

    Completed javascript-loops-practice

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,1 +1,10 @@
 /* exported filterOutNulls */
+function filterOutNulls(values) {
+  var withoutNulls = [];
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      withoutNulls.push(values[i]);
+    }
+  }
+  return withoutNulls;
+}
--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -1,1 +1,10 @@
 /* exported findIndex */
+function findIndex(array, value) {
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      var indexFound = i;
+      return indexFound;
+    }
+  } return -1;
+}
