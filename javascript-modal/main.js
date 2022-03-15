8d181f53374dae53486f64a39d1b4ed1adc7696e
CTMENDOZA <tiptop1112@gmail.com>
2022-03-15 15:07:37 -0700

    Completed javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,13 +1,15 @@
-
 var blueButton = document.querySelector('.blue-button');
+
 var modalButton = document.querySelector('.row-modal.hidden');
 
 blueButton.addEventListener('click', toModalButton);
-function toModalButton(event) {
-  modalButton.className = 'row modal';
-}
 
 modalButton.addEventListener('click', exitModalButton);
+
+function toModalButton(event) {
+  modalButton.className = 'row-modal';
+}
+
 function exitModalButton(event) {
-  modalButton.className = 'row modal hidden';
+  modalButton.className = 'row-modal hidden';
 }
